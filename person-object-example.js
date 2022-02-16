74f1923756ee038eaeaa58906eb039a88068da07
double-slide <93881358+double-slide@users.noreply.github.com>
2022-02-16 00:27:45 +0000

    Object example using a person and two ways to look up info using the key--bracket notation and dot notation

---
+++ person-object-example.js
@@ -0,0 +1,11 @@
+var sister = {
+  name: "Sarah", 
+  age: 23,
+  parents: [ "alice", "andy" ],
+  siblings: ["julia"],
+  favoriteColor: "purple",
+  pets: true
+};
+
+console.log(sister.parents);
+console.log(sister["parents"]);
