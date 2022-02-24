d913baf143ed7a17279830233a4a83ad18bf80dd
double-slide <93881358+double-slide@users.noreply.github.com>
2022-02-24 06:31:02 +0000

    Loopy Lighthouse exercise but in function form -- function receives arrays for start/end, numbers to replace and words to replace them with.

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -1,21 +1,26 @@
 // original code--logs every number between start/finish
 // replaces multiples of 3 with 'loopy', multiples of 4 with 'lighthouse' and multiples of 3 and 4 with 'loopyLighthouse'
 
-for (let x = 100; x <= 200; x++) {
-  if (x % 3 === 0 && x % 4 !== 0) {
-    console.log("Loopy");
-  } else if (x % 3 !== 0 && x % 4 === 0) {
-    console.log("Lighthouse");
-  } else if (x % 3 === 0 && x % 4 === 0) {
-    console.log("LoopyLighthouse");
-  } else {
-    console.log(x);
-  }
-}
 
 
 // new challenge--wrap this code into a function called loopyLighthouse(range, multiples, words)
 // range is array of numbers representing start/end of the loop
 // multiples should be array of 2 numbers 
 
-function 
+function loopyLighthouse(range, multiples, words) {
+
+  for (let x = range[0]; x <= range[1]; x++) {
+    if (x % multiples[0] === 0 && x % multiples[1] !== 0) {
+      console.log(words[0]);
+    } else if (x % multiples[0] !== 0 && x % multiples[1] === 0) {
+      console.log(words[1]);
+    } else if (x % multiples[0] === 0 && x % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else {
+      console.log(x);
+    }
+  }
+  return;
+}
+
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
