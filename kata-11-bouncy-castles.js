9b7ccf043bad4761fce433b6f6c2e967f587e7bf
double-slide <93881358+double-slide@users.noreply.github.com>
2022-03-02 22:01:00 +0000

    kata 11: functions to receive dimensions and return volume of shapes

---
+++ kata-11-bouncy-castles.js
@@ -0,0 +1,52 @@
+// Use the value below whenever you need the value of Pi
+const PI = 3.14159 ;
+
+const sphereVolume = function (radius) {
+  return (4/3) * PI * radius * radius * radius;
+}
+
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+
+
+const coneVolume = function (radius, height) {
+  return PI * radius * radius * height / 3;
+}
+
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
+
+const prismVolume = function (height, width, depth) {
+return height * width * depth;
+}
+
+console.log(prismVolume(3, 4, 5) === 60);
+
+
+const totalVolume = function (solids) {
+return sphereVolume(duck[0].radius) + sphereVolume(duck[1].radius) + coneVolume(duck[2].radius, duck[2].height);
+}
+
+
+const largeSphere = {
+  type: 'sphere',
+  radius: 40
+}
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10
+}
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5
+}
+
+const duck = [
+  largeSphere,
+  smallSphere,
+  cone
+]
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
+
