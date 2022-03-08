ce76a30e0adf489fc5fe6795f117e9b2c2e45696
double-slide <93881358+double-slide@users.noreply.github.com>
2022-03-08 03:56:07 +0000

    Refactored to reduce size

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -1,26 +1,27 @@
 // original code--logs every number between start/finish
 // replaces multiples of 3 with 'loopy', multiples of 4 with 'lighthouse' and multiples of 3 and 4 with 'loopyLighthouse'
 
-
-
 // new challenge--wrap this code into a function called loopyLighthouse(range, multiples, words)
 // range is array of numbers representing start/end of the loop
-// multiples should be array of 2 numbers 
+// multiples should be array of 2 numbers
 
-function loopyLighthouse(range, multiples, words) {
+const loopyLighthouse = function(range, multiples, words) {
 
   for (let x = range[0]; x <= range[1]; x++) {
-    if (x % multiples[0] === 0 && x % multiples[1] !== 0) {
+    let mult1 = x % multiples[0];
+    let mult2 = x % multiples[1];
+
+    if (mult1 === 0 && mult2 !== 0) {
       console.log(words[0]);
-    } else if (x % multiples[0] !== 0 && x % multiples[1] === 0) {
+    } else if (mult1 !== 0 && mult2 === 0) {
       console.log(words[1]);
-    } else if (x % multiples[0] === 0 && x % multiples[1] === 0) {
+    } else if (mult1 === 0 && mult2 === 0) {
       console.log(words[0] + words[1]);
     } else {
       console.log(x);
     }
   }
   return;
-}
+};
 
 console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
